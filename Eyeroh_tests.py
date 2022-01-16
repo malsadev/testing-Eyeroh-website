2c7d72e1304b407143cdd7ef40d0d523d9b1869a
Mustafa Al-Sakkaf <malsa080@uottawa.ca>
2022-01-16 00:37:31 -0500

    finalized test case for index page

--- Eyeroh_tests.py
+++ Eyeroh_tests.py
@@ -3,45 +3,27 @@ from selenium.webdriver.common.keys import Keys
 import unittest
 
 
-
+#run using cmd; type py -m unittest Eyeroh_tests.py
 class EyerohIndexPageTest(unittest.TestCase):
-
+    @classmethod
     def setupClass(cls):
         cls.driver = webdriver.Chrome('./chromedriver')
         cls.driver.implicitly_wait(10)
-        cls.driver.maximize_window()
-
-    def setUp(self):
-        driver.get("https://six-guys.github.io/")
-
-
+        
     def test_click_team_button(self):
-        self.home_btn = driver.find_element_by_xpath("/html/body/div[1]/a[2]")
+        self.home_btn = self.driver.find_element_by_xpath("/html/body/div[1]/a[2]")
         self.home_btn.click()
 
     def test_navigate_to_contact_page(self):
-        self.contact_button = driver.find_element_by_name("Contact")
+        self.contact_button = self.driver.find_element_by_name("Contact")
         self.contact_button.click()
 
+    def tearDown(self):
+        self.driver.get("https://six-guys.github.io/")
 
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+        cls.driver.quit()
 
-
-
-driver = webdriver.Chrome('./chromedriver')
-
-
-driver.get("https://six-guys.github.io/")
-
-home_btn = driver.find_element_by_xpath("/html/body/div[1]/a[2]")
-
-driver.implicitly_wait
-home_btn.click()
-
-
-
-
-
-
-
-#driver.close()
 
